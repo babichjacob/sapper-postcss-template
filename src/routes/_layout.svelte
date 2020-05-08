9d1fe76113bc7f3ca7172776734a7a44742ba92f
Jacob Babich <jacobbabichpublic+git@gmail.com>
2020-05-08 01:16:31 -0400

    🐛 fix: silence Layout segment warnings

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import { stores } from "@sapper/app";
 
+	// You may not want to use `segment`, but it is passed for the time being and will
+	// create a warning if not expected: https://github.com/sveltejs/sapper-template/issues/210
+	// https://github.com/sveltejs/sapper/issues/824
+	export let segment;
+	// Silence unused export property warning
+	if (segment) {};
+
 	const { page } = stores();
 	$: path = $page.path.slice(1);
 </script>
