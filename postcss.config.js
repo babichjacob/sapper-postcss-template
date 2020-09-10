b5ceec6a5d1aaf273424c57cb5e189fe87905351
Jacob Babich <jacobbabichpublic+git@gmail.com>
2020-09-10 14:35:36 -0400

    ⚡️ feat: remove `postcss-preset-env` because it's semi-unmaintained; advice: use the stuff it contains directly instead of it

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 const cssnano = require("cssnano");
 const postcssImport = require("postcss-import");
-const postcssPresetEnv = require("postcss-preset-env");
 const postcssPurgecss = require("@fullhuman/postcss-purgecss");
 const tailwindcss = require("tailwindcss");
 const tailwindcssConfig = require("./tailwind.config");
@@ -14,12 +13,7 @@ module.exports = {
 
 		tailwindcss(tailwindcssConfig),
 
-		postcssPresetEnv({
-			features: {
-				// https://github.com/tailwindcss/tailwindcss/issues/1190
-				"focus-within-pseudo-class": false,
-			},
-		}),
+		// More plugins for polyfills and the like (such as postcss-preset-env) should generally go here
 
 		!dev && postcssPurgecss({
 			content: ["./src/**/*.svelte", "./src/**/*.html"],
